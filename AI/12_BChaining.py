00a2c40d8965e3333a989c7ef508bf4b9f59c91a
Anant <90376578+Antverse1@users.noreply.github.com>
2023-11-08 22:00:58 +0530

    Minor Change

--- AI/12_BChaining.py
+++ AI/12_BChaining.py
@@ -25,17 +25,17 @@ knowledge_base = {
 def backward_chaining(goal, known_facts):
     if goal in known_facts:
         return True
-    for ruel, value in knowledge_base.items():
+    for rule, value in knowledge_base.items():
         if goal in value["if"]:
             all_conditions_met = all(condition in known_facts for condition in value["if"])
             if all_conditions_met and backward_chaining(value["then"], known_facts):
                 return True
-        return False
+    return False
 
 
 if __name__ == "__main__":
     goal = "C"
-    known_facts = ["G", "F", "E"]
+    known_facts = ["G", "D", "E"]
 
     if backward_chaining(goal, known_facts):
         print(f"The Goal '{goal}' can be reached")
